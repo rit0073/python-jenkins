dafc02650e64389775c6ae87a604b341dfb00fd1
rit0073 <rit0073@gmail.com>
2021-02-07 23:59:35 +0530

    fixed the test

--- tests/test_main_prog.py
+++ tests/test_main_prog.py
@@ -7,4 +7,4 @@ from main_prog.main_prog import return_today_date
 class DummyTest(unittest.TestCase):
     def test_return_today_date(self):
         now = datetime.datetime.now()
-        self.assertEqual(now.strftime('%d-%m-%Y'), return_today_date())
+        self.assertEqual(now.strftime('%d/%m/%Y'), return_today_date())
